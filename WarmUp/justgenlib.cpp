55a75123f9964f77123f8eac1b2bf64435a20d72
mynorjrv <mynorjrv@gmail.com>
2023-03-12 14:14:41 -0600

    [] Trying to run some programs

---
+++ WarmUp/justgenlib.cpp
@@ -0,0 +1,17 @@
+/*
+* File: justgenlib.cpp
+* --------------
+* This program just imports genlib and prints.
+* I dont know how to initialize the library so...
+* Just trying.
+*/
+
+#include <iostream>
+#include "genlib.h"
+
+int main() 
+{
+
+	cout << "This program just prints this phrase." << endl;
+	return 0;
+}
